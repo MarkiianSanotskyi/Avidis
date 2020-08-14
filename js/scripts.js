e6c49d06478d6406212b257cf5bfa5127180e725
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-08-14 10:47:33 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,295 @@
+/*Fancybox*/
+$(document).ready(function(){
+
+            var forms = $('.form-modal'),
+            cb_input = forms.find('input[type=text], input[type=email], input[type=tel] , textarea'),
+            cr_close = $.fancybox.close();
+
+    $("#fancybox-overlay").fancybox({
+        padding: [0, 0, 0, 0],
+        margin: [120, 0, 0, 0],
+        scrolling: 'visible',
+        minWidth: 450,
+        minHeight: 100,
+        autoSize: false,
+        autoHeight: true,
+        autoWidth: true,
+        maxWidth: 9999,
+        maxHeight: 9999,
+        fixed: false,
+        autoCenter: false,
+        closeBtn: true,
+        afterClose: function () {
+            cb_input.val('');
+        }
+    });
+
+    $('.btr').click(function () {
+        $.fancybox.close();
+    });
+	
+   });
+/*Fancybox end*/  
+ 
+ 
+/*nav*/  
+$(document).ready(function(){
+   $('#header a[href*=#], #counter a[href*=#], #why-working a[href*=#], #price a[href*=#], #as-work a[href*=#], #counter-second a[href*=#], #reviews a[href*=#contact-anchor]').bind("click", function(e){
+      var anchor = $(this);
+   $(anchor).parent().parent().find('li').removeClass('current');
+   $(anchor).parent('li').addClass('current');
+   
+      $('html, body').stop().animate({
+         scrollTop: $(anchor.attr('href')).offset().top
+      }, 800);
+      e.preventDefault();
+   });
+   return false;
+});
+
+/*placeholder*/
+$(document).ready(function(){
+        $.Placeholder.init({ color : "#808080" });
+ });
+
+$(document).ready(function(){
+        var $menu = $(".header-fixed-box");
+   $(window).scroll(function(){
+            if ( $(this).scrollTop() > 15 && $menu.hasClass("default") ){
+        $menu.removeClass("default").addClass("fixed");
+            } else if($(this).scrollTop() <= 15 && $menu.hasClass("fixed")) {
+                $menu.removeClass("fixed").addClass("default");
+            }
+        });//scroll
+    });
+
+	
+	
+function mycarousel_initCallback(carousel) {
+    jQuery('.jcarousel-control a').bind('click', function() {
+        carousel.scroll(jQuery.jcarousel.intval(jQuery(this).text()));
+        return false;
+    });
+
+
+
+    jQuery('#mycarousel-next').bind('click', function() {
+        carousel.next();
+        return false;
+    });
+
+    jQuery('#mycarousel-prev').bind('click', function() {
+        carousel.prev();
+        return false;
+    });
+};
+
+// Ride the carousel...
+jQuery(document).ready(function() {
+    jQuery("#mycarousel").jcarousel({
+        scroll: 2,
+        initCallback: mycarousel_initCallback,
+        // This tells jCarousel NOT to autobuild prev/next buttons
+        buttonNextHTML: null,
+        buttonPrevHTML: null
+    });
+});
+
+
+function initialize() {
+  var myLatlng = new google.maps.LatLng(53.211687,50.20682);
+  var mapOptions = {
+	  disableDefaultUI: true,
+    zoom: 15,
+    center: myLatlng,
+	styles:[
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e9e9e9"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#dedede"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#333333"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    }
+]
+					
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+  });
+}
+
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+ 
